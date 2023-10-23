70adf2b0ee1d7e240caf9922e7e4caeafa0afd66
RobicaCodeaza <stoicarobert38@yahoo.com>
2023-10-23 15:36:30 +0300

    Added Testimonials

--- script.js
+++ script.js
@@ -53,3 +53,106 @@ const previewHome = function (e) {
   home.scrollIntoView({ behavior: 'smooth', block: 'center' });
 };
 homes.addEventListener('click', previewHome);
+
+// -------------------------------------------------------
+
+const player = document.querySelector('lottie-player');
+
+const loadingLottie = async function () {
+  try {
+    await player.load(
+      'https://lottie.host/2c24727f-c0fd-4acd-8d88-3097b9f9a392/zahwGLHCtC.json'
+    );
+    // player.setAttribute('background', 'red');
+    // setTimeout(() => {
+    player.setSpeed(2);
+    player.play();
+    // }, 1000);
+  } catch (err) {
+    console.log(err);
+  }
+};
+loadingLottie();
+const sliders = async function () {
+  try {
+    const testimonials = document.querySelectorAll('.testimonial');
+    const dotsContainer = document.querySelector('.dots');
+    let currentSlide = 0;
+    let maxSlide = testimonials.length - 1;
+    const btnPreviousTestimonial = document.querySelector(
+      '.testimonials-box__sidebar__btn--previous'
+    );
+    const btnNextTestimonial = document.querySelector(
+      '.testimonials-box__sidebar__btn--next'
+    );
+
+    const createDots = function () {
+      testimonials.forEach((_, i) => {
+        const dot = `<button class="dots__dot" data-slide="${i}"></button>`;
+        dotsContainer.insertAdjacentHTML('beforeend', dot);
+      });
+    };
+
+    const activateDot = function (slide) {
+      const dots = document.querySelectorAll('.dots__dot');
+      dots.forEach((dot) => dot.classList.remove('dots__dot--active'));
+      const currentDot = document.querySelector(
+        `.dots__dot[data-slide ="${slide}" ]`
+      );
+      currentDot.classList.add('dots__dot--active');
+    };
+
+    const goToSlide = async function (slide) {
+      try {
+        testimonials.forEach(
+          (card, i) =>
+            (card.style.transform = `translateY(${(i - slide) * 100}%)`)
+        );
+        player.stop();
+        player.play();
+      } catch (err) {
+        throw err;
+      }
+    };
+
+    const nextSlide = function () {
+      console.log(currentSlide, maxSlide);
+      if (currentSlide === maxSlide) {
+        console.log('entered');
+        currentSlide = 0;
+      } else currentSlide++;
+      console.log(currentSlide, maxSlide);
+      goToSlide(currentSlide);
+      activateDot(currentSlide);
+    };
+
+    const previousSlide = function () {
+      if (currentSlide === 0) currentSlide = maxSlide;
+      else currentSlide--;
+      goToSlide(currentSlide);
+      activateDot(currentSlide);
+    };
+
+    const init = function () {
+      goToSlide(0);
+      createDots();
+      activateDot(0);
+    };
+    init();
+
+    btnNextTestimonial.addEventListener('click', nextSlide);
+    btnPreviousTestimonial.addEventListener('click', previousSlide);
+    dotsContainer.addEventListener('click', function (e) {
+      if (e.target.classList.contains('dots__dot')) {
+        const { slide } = e.target.dataset;
+        currentSlide = slide;
+        goToSlide(currentSlide);
+        activateDot(currentSlide);
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+sliders();
